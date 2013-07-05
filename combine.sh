0612eefedc9062e51262e8aac93884d7d549022a
Rogério Brito <rbrito@ime.usp.br>
2013-07-05 12:43:19 -0300

    Add exception rules.

--- combine.sh
+++ combine.sh
@@ -2,4 +2,4 @@
 
 # Prepend headers first
 echo "[Adblock]" > adblock-rules.txt
-cat ads.txt annoyances.txt tracking.txt unsorted.txt hiding.txt >> adblock-rules.txt
+cat ads.txt annoyances.txt tracking.txt unsorted.txt hiding.txt exception.txt >> adblock-rules.txt
