d0d48394a85ea1ecc19aa1aeea59052b01f2345f
Rogério Brito <rbrito@ime.usp.br>
2013-07-05 14:56:00 -0300

    Reorganize the structure of the project.

--- combine.sh
+++ combine.sh
@@ -2,4 +2,9 @@
 
 # Prepend headers first
 echo "[Adblock]" > adblock-rules.txt
-cat ads.txt annoyances.txt tracking.txt unsorted.txt hiding.txt exception.txt >> adblock-rules.txt
+cat partial-lists/ads.txt \
+    partial-lists/annoyances.txt \
+    partial-lists/tracking.txt \
+    partial-lists/unsorted.txt \
+    partial-lists/hiding.txt \
+    partial-lists/exception.txt >> adblock-rules.txt
